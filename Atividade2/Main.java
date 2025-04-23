c4c322577d87b6133e1c58bb1be4532455344b3b
Lucas <lucas_rhodensantos@hotmail.com>
2025-04-23 17:59:24 -0300

    Atv Olamundo pt2

--- Atividade2/Main.java
+++ Atividade2/Main.java
@@ -3,7 +3,7 @@ package Atividade2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Ola Mundo");
     }
 }
 
