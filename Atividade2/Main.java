007ef11bc842337dd6772231d69e278dfbb9f0c6
Lucas <lucas_rhodensantos@hotmail.com>
2025-04-23 18:02:03 -0300

    HolaQueTal

--- Atividade2/Main.java
+++ Atividade2/Main.java
@@ -3,7 +3,7 @@ package Atividade2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
+        System.out.println("Hola que tal");
     }
 }
 
